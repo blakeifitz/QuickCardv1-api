581a93a05b5a59f780591e53aad661b87d0147f4
blakeifitz <blakeifitz@gmail.com>
2021-01-06 22:46:40 -0500

    cors allow cross origin

--- src/app.js
+++ src/app.js
@@ -15,8 +15,13 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://quick-cardv1-client-jav1v75nw.vercel.app/");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use("/api/auth", authRouter)
-app.options('*', cors())
 app.use("/api/deck", deckRouter);
 app.use("/api/card", cardRouter);
 app.use("/api/users", usersRouter);
