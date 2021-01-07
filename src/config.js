119597ac122b22963eb6d9c8bac51a0077eb70b3
blakeifitz <blakeifitz@gmail.com>
2021-01-06 22:20:27 -0500

    database url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'dev',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://blakefitzpatrick@localhost/postgres',
-    JWT_SECRET: process.env.JWT_SECRET || 'josieSally',
+    DATABASE_URL: process.env.DATABASE_URL || 'TEST_DATABASE_URL',
+    JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
   }
