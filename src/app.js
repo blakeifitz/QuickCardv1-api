0ae702f88605b4173631772a33fd9e32eb4ae266
blakeifitz <blakeifitz@gmail.com>
2021-01-06 22:51:14 -0500

    typo

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://quick-cardv1-client-jav1v75nw.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://quick-cardv1-client-jav1v75nw.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
